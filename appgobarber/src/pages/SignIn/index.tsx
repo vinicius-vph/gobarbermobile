9961d9b3fc3a84abf662b996c3930ac2503a4327
Vinicius Santos <vinicius.vph@gmail.com>
2021-01-15 20:17:47 -0300

    finish authentication screen

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
-
 import logoImg from '../../assets/logo.png';
+
+import {
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreatAccountButton,
+    CreateAccountButtonText
+} from './styles';
+
 
 const SignIn: React.FC = () => {
     return(
-        <Container>
-            <Image source={logoImg} />
+        <>
+            <KeyboardAvoidingView
+                style= {{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    contentContainerStyle={{ flex: 1 }}
+                >
+                    <Container>
+                        <Image source={logoImg} />
 
-            <Title>Faça seu login</Title>
+                        <View>
+                            <Title>Faça seu login</Title>
+                        </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail"/>
-            <Input name="password" icon="lock" placeholder="Senha"/>
-
-            <Button onPress={() => {}}>Entrar</Button>
-        </Container>
+                        <Input name="email" icon="mail" placeholder="E-mail"/>
+                        <Input name="password" icon="lock" placeholder="Senha"/>
+         
+                        <Button onPress={() => {}}>Entrar</Button>
+                        <ForgotPassword onPress={() => {}}>
+                            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <CreatAccountButton onPress={() => {}}>
+                <Icon name="log-in" size={20} color="ff9000" />
+                <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+            </CreatAccountButton>
+        </>
     );
 };
 
