5c715999c290ba6e36f378193a6bea04de97613b
Vinicius Santos <vinicius.vph@gmail.com>
2021-01-19 20:03:08 -0300

    add functions next and send, values and keyboards patterns of inputs

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,5 +1,12 @@
 import React, {useRef} from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+    Image,
+    View,
+    ScrollView,
+    KeyboardAvoidingView,
+    Platform,
+    TextInput
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
@@ -20,8 +27,10 @@ import {
 
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
-
     const navigation = useNavigation();
+
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
 
     return(
         <>
@@ -45,11 +54,44 @@ const SignUp: React.FC = () => {
                             console.log(data);
                         }}>
 
-                            <Input name="name" icon="user" placeholder="Nome"/>
+                            <Input
+                                autoCapitalize="words"
+                                name="name"
+                                icon="user"
+                                placeholder="Nome"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    emailInputRef.current.focus();
+                                }}
 
-                            <Input name="email" icon="mail" placeholder="E-mail"/>
+                            />
 
-                            <Input name="password" icon="lock" placeholder="Senha"/>
+                            <Input
+                                ref={emailInputRef}
+                                keyboardType="email-address"
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current.focus();
+                                }}
+
+                            />
+
+                            <Input
+                                ref={passwordInputRef}
+                                secureTextEntry
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() => formRef.current?.submitForm()}
+
+                            />
 
                             <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
                         </Form>
@@ -58,7 +100,7 @@ const SignUp: React.FC = () => {
                 </ScrollView>
             </KeyboardAvoidingView>
             <BackToSignIn onPress={() => navigation.goBack()}>
-                <Icon name="arrow-left" size={20} color="fff" />
+                <Icon name="arrow-left" size={20} color="#fff" />
                 <BackToSignInText>Voltar para o login</BackToSignInText>
             </BackToSignIn>
         </>
